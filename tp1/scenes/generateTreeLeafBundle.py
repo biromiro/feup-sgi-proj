b5435eacc659a59f19e20882969dc7b7343c264e
Joao Baltazar <up201905616@edu.fe.up.pt>
2022-10-19 10:59:48 +0100

    Correct tree bundle primitives

--- tp1/scenes/generateTreeLeafBundle.py
+++ tp1/scenes/generateTreeLeafBundle.py
@@ -1,6 +1,7 @@
 import numpy as np
 import itertools as it
 import sys
+from regex import B
 from scipy import spatial as sp
 
 
@@ -34,11 +35,23 @@ def generateBundle(numPoints, typeOfBundle):
     leafTriangles = genTriangles(points)
 
     for idx, trio in enumerate(leafTriangles):
-        (x1, y1, z1) = points[trio[0]]
-        (x2, y2, z2) = points[trio[1]]
-        (x3, y3, z3) = points[trio[2]]
+        a = points[trio[0]]
+        b = points[trio[1]]
+        c = points[trio[2]]
+
+        (x1, y1, z1) = a
+        (x2, y2, z2) = b 
+        (x3, y3, z3) = c
+
+        ab = (x2-x1, y2-y1, z2-z1)
+        bc = (x3-x2, y3-y2, z3-z2)
+        normal = np.cross(ab, bc)
+        centroid = ((x1+x2+x3)/3, (y1+y2+y3)/3, (z1+z2+z3)/3)
+        if (np.dot(normal, centroid) < 0):
+            (x1,y1,z1), (x3, y3, z3) = c, a
         print(
-            f"<primitive id=\"{typeOfBundle}Triangle{idx}\"> \n\t<triangle x1=\"{x1}\" y1=\"{y1}\" z1=\"{z1}\" x2=\"{x2}\" y2=\"{y2}\" z2=\"{z2}\" x3=\"{x3}\" y3=\"{y3}\" z3=\"{z3}\" /> \n</primitive>")
+           f"<primitive id=\"{typeOfBundle}Triangle{idx}\"> \n\t<triangle x1=\"{x1}\" y1=\"{y1}\" z1=\"{z1}\" x2=\"{x2}\" y2=\"{y2}\" z2=\"{z2}\" x3=\"{x3}\" y3=\"{y3}\" z3=\"{z3}\" /> \n</primitive>")
+
 
     print("\n\n\n")
     print(f"<component id=\"{typeOfBundle}\"> \n\t<transformation>\n\t</transformation>\n\t<materials>\n\t\t<material id=\"inherit\" />\n\t</materials>\n\t<texture id=\"none\" />\n\t<children>")
